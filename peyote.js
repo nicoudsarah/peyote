1a3eba106c1c79c0c7e7f514460257d7e8fdcd57
nicoudsarah <sarah@sogilis.com>
2020-06-15 13:59:11 +0200

    [REFACTOR] Peyote : improve odd row by adding two functions: createANewCell and create ANewOddRow

--- peyote.js
+++ peyote.js
@@ -1,24 +1,34 @@
 var diagram = document.getElementById("grid");
 
+function CreateANewOddRow() {
+    var oddRow = document.createElement("div");
+    oddRow.className = "odd-row";
+
+    for (let nbOfCell = 0; nbOfCell < 6; nbOfCell++){
+        var cell = addANewCell();
+        oddRow.appendChild(cell);
+    }
+    
+    /*var cell = addANewCell();
+
+    var cell2 = cell.cloneNode(true);
+    var cell3 = cell.cloneNode(true);
+    var cell4 = cell.cloneNode(true);
+    var cell5 = cell.cloneNode(true);
+    oddRow.appendChild(cell);
+    oddRow.appendChild(cell2);
+    oddRow.appendChild(cell3);
+    oddRow.appendChild(cell4);
+    oddRow.appendChild(cell5);*/
+
+    return oddRow;
+}
+
 for (let nbOfLine = 0; nbOfLine < 5 ; nbOfLine++) {
 
     if (numIsPair(nbOfLine) == false){
-        var oddRow = document.createElement("div");
-        oddRow.className = "odd-row";
 
-        var cell = document.createElement("div");
-        cell.className = "cell";
-
-        var cell2 = cell.cloneNode(true);
-        var cell3 = cell.cloneNode(true);
-        var cell4 = cell.cloneNode(true);
-        var cell5 = cell.cloneNode(true);
-
-        oddRow.appendChild(cell);
-        oddRow.appendChild(cell2);
-        oddRow.appendChild(cell3);
-        oddRow.appendChild(cell4);
-        oddRow.appendChild(cell5);
+        var oddRow = CreateANewOddRow();
 
         diagram.appendChild(oddRow);
     }
@@ -27,15 +37,14 @@ for (let nbOfLine = 0; nbOfLine < 5 ; nbOfLine++) {
         var pairRow = document.createElement("div");
         pairRow.className = "pair-row";
 
-        var cell1bis = document.createElement("div");
-        cell1bis.className = "cell";
+        var cell = addANewCell();
 
-        var cell2bis = cell1bis.cloneNode(true);
-        var cell3bis = cell1bis.cloneNode(true);
-        var cell4bis = cell1bis.cloneNode(true);
-        var cell5bis = cell1bis.cloneNode(true);
+        var cell2bis = cell.cloneNode(true);
+        var cell3bis = cell.cloneNode(true);
+        var cell4bis = cell.cloneNode(true);
+        var cell5bis = cell.cloneNode(true);
 
-        pairRow.appendChild(cell1bis);
+        pairRow.appendChild(cell);
         pairRow.appendChild(cell2bis);
         pairRow.appendChild(cell3bis);
         pairRow.appendChild(cell4bis);
@@ -48,6 +57,12 @@ for (let nbOfLine = 0; nbOfLine < 5 ; nbOfLine++) {
 
 function numIsPair (num) {
     return (num & 1) ? true : false ;
+}
+
+function addANewCell() {
+    var cell = document.createElement("div");
+    cell.className = "cell";
+    return cell;
 }
 
 
