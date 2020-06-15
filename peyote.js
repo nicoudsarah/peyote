796346470f5d898619d1e54b462f38ee2b0ce2c3
nicoudsarah <sarah@sogilis.com>
2020-06-15 14:07:02 +0200

    [REFACTOR] Peyote: improve pair row by adding the functions createANewCell() and createANewPairRow()

--- peyote.js
+++ peyote.js
@@ -1,29 +1,5 @@
 var diagram = document.getElementById("grid");
 
-function CreateANewOddRow() {
-    var oddRow = document.createElement("div");
-    oddRow.className = "odd-row";
-
-    for (let nbOfCell = 0; nbOfCell < 6; nbOfCell++){
-        var cell = addANewCell();
-        oddRow.appendChild(cell);
-    }
-    
-    /*var cell = addANewCell();
-
-    var cell2 = cell.cloneNode(true);
-    var cell3 = cell.cloneNode(true);
-    var cell4 = cell.cloneNode(true);
-    var cell5 = cell.cloneNode(true);
-    oddRow.appendChild(cell);
-    oddRow.appendChild(cell2);
-    oddRow.appendChild(cell3);
-    oddRow.appendChild(cell4);
-    oddRow.appendChild(cell5);*/
-
-    return oddRow;
-}
-
 for (let nbOfLine = 0; nbOfLine < 5 ; nbOfLine++) {
 
     if (numIsPair(nbOfLine) == false){
@@ -33,22 +9,7 @@ for (let nbOfLine = 0; nbOfLine < 5 ; nbOfLine++) {
         diagram.appendChild(oddRow);
     }
     else {
-        console.log(numIsPair(nbOfLine));
-        var pairRow = document.createElement("div");
-        pairRow.className = "pair-row";
-
-        var cell = addANewCell();
-
-        var cell2bis = cell.cloneNode(true);
-        var cell3bis = cell.cloneNode(true);
-        var cell4bis = cell.cloneNode(true);
-        var cell5bis = cell.cloneNode(true);
-
-        pairRow.appendChild(cell);
-        pairRow.appendChild(cell2bis);
-        pairRow.appendChild(cell3bis);
-        pairRow.appendChild(cell4bis);
-        pairRow.appendChild(cell5bis);
+        var pairRow = CreateANewPairRow();
 
         diagram.appendChild(pairRow);
     }
@@ -57,6 +18,30 @@ for (let nbOfLine = 0; nbOfLine < 5 ; nbOfLine++) {
 
 function numIsPair (num) {
     return (num & 1) ? true : false ;
+}
+
+function CreateANewOddRow() {
+    var oddRow = document.createElement("div");
+    oddRow.className = "odd-row";
+
+    for (let nbOfCell = 0; nbOfCell < 5; nbOfCell++){
+        var cell = addANewCell();
+        oddRow.appendChild(cell);
+    }
+
+    return oddRow;
+}
+
+function CreateANewPairRow() {
+    var pairRow = document.createElement("div");
+    pairRow.className = "pair-row";
+
+    for (let nbOfCell = 0; nbOfCell < 5; nbOfCell++){
+        var cell = addANewCell();
+        pairRow.appendChild(cell);
+    }
+
+    return pairRow;
 }
 
 function addANewCell() {
